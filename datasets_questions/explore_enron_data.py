9c320f0618fab3fac10f9b6994154324b73d8bd4
svelamoor <svelamoor@me.com>
2018-05-15 17:23:45 +0000

    Minor updates

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,28 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print enron_data.items() 
+#print len(enron_data)
+print len(enron_data["SKILLING JEFFREY K"])
 
+pois = {k:v for (k,v) in enron_data.iteritems() if v['poi'] == True}
+print "Persons of Interest -- ", len(pois)
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+print enron_data["COLWELL WESLEY"]['from_this_person_to_poi']
+print enron_data["SKILLING JEFFREY K"]['exercised_stock_options']
+print "Skilling ", enron_data["SKILLING JEFFREY K"]['total_payments']
+print "Fastow ", enron_data["FASTOW ANDREW S"]['total_payments']
+print "Lay ", enron_data["LAY KENNETH L"]['total_payments']
+
+
+def findNaNs(dict, field):
+	return len([k for (k,v) in dict.items() if v[field] == 'NaN'])
+
+defined_salary = [k for (k,v) in enron_data.items() if v['salary'] != 'NaN']
+known_email_address = [k for (k,v) in enron_data.items() if v['email_address'] != 'NaN']
+
+print "Salary ", len(defined_salary), " known email address - ", len(known_email_address)
+
+print "Unknown total payments for ", findNaNs(enron_data, 'total_payments'), " people which represents ", 1.0*findNaNs(enron_data, 'total_payments')/len(enron_data)*100.0 , " percentage of all of the dataset"
+print "Unknown total payments for POIS ", findNaNs(pois, 'total_payments'), " people which represents ", 1.0*findNaNs(pois, 'total_payments')/len(pois)*100.0 , " percentage of all POIs"
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+clf = SVC(kernel = "rbf", C=10000.0)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time ", round(time()-t0, 3)," seconds"
+pred = clf.predict(features_test)
+print "accuracy score ", accuracy_score(labels_test, pred)
+print "10th ", "Chris" if pred[10] == 1 else "Sara"
+print "26th ", "Chris" if pred[26] == 1 else "Sara"
+print "50th ", "Chris" if pred[50] == 1 else "Sara"
+print "Chris Emails predicted ", len([x for x in pred if x == 1])
+print "Sara Emails predicted ", len([x for x in pred if x == 0])
 #########################################################
 
 
